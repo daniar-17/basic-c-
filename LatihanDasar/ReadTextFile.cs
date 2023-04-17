eb28231b84c5141e778200371fa305b2d2077e7f
daniar-17 <daniarnuramin1@gmail.com>
2023-04-17 09:54:58 +0700

    Update Read and Write

--- LatihanDasar/ReadTextFile.cs
+++ LatihanDasar/ReadTextFile.cs
@@ -9,17 +9,46 @@ namespace LatihanDasar
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Read Text File");
-            string folder = @"C:\DANIAR\LATE01\DANIAR N\JobSeeker\C# Latihan\Latihan Dasar\LatihanDasar\";
-            string fileName = "Day.txt";
-            string fullPath = folder + fileName;
-            string[] lines = File.ReadAllLines(fullPath);
-            for(int i = 0; i < lines.Length; i++)
+            try
             {
-                Console.WriteLine($"Kata : {i} - {lines[i]}");
-                string[] kata = lines[i].Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries);
+                Console.WriteLine("Read Text File");
+                string folder = @"C:\DANIAR\LATE01\DANIAR N\JobSeeker\C# Latihan\Latihan Dasar\LatihanDasar\";
+                string fileName = "startPrice.txt";
+                string fileName2 = "finalPrice.txt";
+                string fullPath = folder + fileName;
+                string fullPath2 = folder + fileName2;
+                string[] lines = File.ReadAllLines(fullPath);
+                List<string> list = new List<string>();
+                string[] final;
+                int tempInt = 0;
+                string tempString = "";
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    //Console.WriteLine($"Kata : {i} - {lines[i]}");
+                    string[] kata = lines[i].Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                    tempInt = int.Parse(kata[1]) * int.Parse(kata[2]);
+                    if (kata[3] == "EUR")
+                    {
+                        tempInt = tempInt * 19000;
+                    }
+                    else if (kata[3] == "USD")
+                    {
+                        tempInt = tempInt * 14000;
+                    }
+                    tempString = $"{kata[0]}, {kata[1]}, {kata[2]}, {kata[3]}, {tempInt}";
+                    list.Add(tempString);
+                    tempString = "";
+                    tempInt = 0;
+                }
+                final = list.ToArray();
+                File.WriteAllLines(fullPath2, final);
+                Console.ReadLine();
             }
-            Console.ReadLine();
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        //Last Line
         }
     }
 }
